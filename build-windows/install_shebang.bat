3627de9129630e07275ed95ecf6807fa9271d8e7
zwetan <zwetan@gmail.com>
2016-02-01 23:53:27 +0000

    remove old builds
    git-svn-id: http://dev.corsaair.com/svn/as3shebang/trunk@35 78535315-fd31-4a46-8ef3-8a7b7d553742

--- build-windows/install_shebang.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem associate the empty extension with shebangfile file type
-assoc .=shebangfile
-
-rem define filte type "shebangfile" to run with shebang.bat
-ftype shebangfile=shebang.bat "%1" %*
-
-rem add the empty extension to path extension
-set PATHEXT=%PATHEXT%;.
